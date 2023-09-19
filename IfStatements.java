b6576f7d1b4140baf472f02137fb610ba1aefbec
EvaBartolome <eva.bartolome@mrjaffesclass.com>
2023-09-18 20:32:22 -0700

    commit message

---
+++ IfStatements.java
@@ -0,0 +1,36 @@
+
+public class IfStatements{
+    public static void main(String[] args)
+    {
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d= -8; 
+     if (a==5) {
+        System.out.println("SUCCESS");
+        } else {
+        System.out.println("FAILURE");
+    }
+     
+     if (b==2) {
+        System.out.println("SUCCESS");
+    } else {
+        System.out.println("FAILURE");
+    }
+    
+    if (c !=1) {
+        System.out.println("SUCCESS");
+    } else {
+        System.out.println("FAILURE");
+    }
+    
+    if (d == -8 && a == 4) {
+        System.out.println("SUCCESS");
+    } else {
+        System.out.println("FAILURE");
+    }
+}
+}
+
+
+
